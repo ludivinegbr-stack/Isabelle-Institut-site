15d0f8c871c8ddccb3d6fa00ed8bec772ead64ad
ludivinegbr-stack <ludivine.gbr@gmail.com>
2026-05-10 11:00:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Isabelle-Institut-site/',
 })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
